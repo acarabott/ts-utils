3c4eb3ee4843333a1ffcb112d5adeccd1f5276b6
Arthur Carabott <arthur.carabott@output.com>
2019-11-27 13:48:53 -0800

    gestureStream: even more example typing

--- src/gestureCanvas.ts
+++ src/gestureCanvas.ts
@@ -2,16 +2,26 @@ import { canvas } from "@thi.ng/hdom-canvas";
 import { StreamMerge } from "@thi.ng/rstream";
 import { labeled } from "@thi.ng/transducers";
 
-import { gestureStream, IGestureStreamOptions } from "./gestureStream";
+import {
+  GestureEvent,
+  gestureStream,
+  IGestureStreamOptions,
+} from "./gestureStream";
 
 // Canvas creation helper
 // -----------------------------------------------------------------------------
 
 // adds an init lifecycle method to the hdom-canvas, so that geatureStreams can
 // be added once the element is created
-export const gestureCanvas = <T>(
-  parentStream: StreamMerge<any, T>,
-  gestureId: string,
+export type LabeledGestureStream<T extends string> = [T, GestureEvent[]];
+export type GestureStream<T extends string> = StreamMerge<
+  any,
+  LabeledGestureStream<T>
+>;
+
+export const gestureCanvas = <T_ID extends string>(
+  parentStream: StreamMerge<any, LabeledGestureStream<T_ID>>,
+  gestureId: T_ID,
   options?: Partial<IGestureStreamOptions>,
 ) => ({
   ...canvas,
