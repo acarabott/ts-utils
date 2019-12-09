aa5ccc2badfd7b455c4d7937c690718c8f798542
Arthur Carabott <arthur.carabott@output.com>
2019-12-09 11:46:17 -0800

    fix semitonesToDetune name

--- src/semitonesToDetune.ts
+++ src/semitonesToDetune.ts
@@ -1,5 +1,5 @@
 // Converts a value in semitones into a multiplier
 // e.g. 0st = 1.0, 7st = 1.583, 12st = 2.0, -5st = 0.583
 // Used for detuning
-export const semitonesToMultiplier = (semitones: number) =>
+export const semitonesToDetune = (semitones: number) =>
   1.0 + (1 / 12) * semitones;
